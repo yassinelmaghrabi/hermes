e0d828c01fbf94a4de72fe03964848eabbf9031f
yassinelmaghrabi <yassin.the.mega2@gmail.com>
2024-10-17 18:23:53 +0300

    removed cookies for now

--- backend/database/taskmodel.go
+++ backend/database/taskmodel.go
@@ -52,7 +52,7 @@ func GetTask(id primitive.ObjectID, userid primitive.ObjectID) (Task, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := collection.FindOne(ctx, bson.M{"_id": id,"userid":userid}).Decode(&task)
+	err := collection.FindOne(ctx, bson.M{"_id": id, "userid": userid}).Decode(&task)
 	return task, err
 }
 
