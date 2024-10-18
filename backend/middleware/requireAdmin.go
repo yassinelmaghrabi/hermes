83ffa7d0333e505e427d04ef2434a8f74fec096a
apdo60311 <apdo60311@gmail.com>
2024-10-18 15:23:45 -0400

    fixed more missing returns

--- backend/middleware/requireAdmin.go
+++ backend/middleware/requireAdmin.go
@@ -13,14 +13,17 @@ func RequireAdmin(c *gin.Context) {
 		user = val.(database.User)
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "????"})
+		return
 	}
 	user, err := database.GetUserByID(user.ID)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "not a real user"})
+		return
 	}
 	if user.Privilege != 2 {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "not an admin"})
-	} else {
-		c.Next()
+		return
 	}
+	c.Next()
+
 }
