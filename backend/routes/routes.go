8e718e9b0534bb911900f08f1630cfcaa1a1347f
yassinelmaghrabi <yassin.the.mega2@gmail.com>
2024-10-20 22:37:12 +0300

    Abdelhamed should not TOUCH THE ROUTES

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -47,9 +47,8 @@ func RegisterRoutes(router *gin.Engine) {
 	}
 
 	authapi := router.Group("/api/auth")
-	authapi.Use(middleware.AuthenticationMiddleware())
 	{
-		authapi.POST("/add", middleware.AuthorizationMiddleware(database.UserRole.Admin), controllers.CreateUser)
+		authapi.POST("/add", controllers.CreateUser)
 		authapi.POST("/login", controllers.Login)
 		authapi.POST("/requestResetpassword", controllers.RequestResetPassword)
 		authapi.POST("/resetpassword", controllers.ResetPassword)
